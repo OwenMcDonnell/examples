d8a71cb57a02a3309a88da7392acd168f4c3dac0
Owen McDonnell <omcdonnell@gmail.com>
2021-02-09 22:18:32 -0800

    add grid, clean grid results upon new search

--- node/music-search/index.js
+++ node/music-search/index.js
@@ -43,15 +43,47 @@ if ('album' in args) {
     
     let p = await pglet.page();
 
+    // await p.send(`
+    //     add
+    //     stack horizontal horizontalAlign='stretch'
+    //         stack width='100%' horizontal horizontalAlign='stretch'
+    //             grid compact=false selection=none preserveSelection headerVisible=true
+    //                 columns
+    //                     column onClick name='File Type' icon=Page iconOnly fieldName='iconName' minWidth=20 maxWidth=20
+    //                         icon name=FileTemplate size=16
+    //                     column resizable sortable name='Name' fieldName='name'
+    //                         text value='{name}'
+    //                     column resizable name='Write' fieldName='write'
+    //                         stack horizontal height='100%' verticalAlign=center
+    //                             checkbox value='{write}'
+    //                     column resizable name='Color' fieldName='read'
+    //                         dropdown value='{color}' data='{key}'
+    //                             option key=red text='Red'
+    //                             option key=green text='Green'
+    //                             option key=blue text='Blue'
+    //                     column resizable sortable name='Description' fieldName='description'
+    //                         textbox value='{description}'
+    //                     column sortable=number name='Action' fieldName='key' minWidth=150
+    //                         stack horizontal height='100%' verticalAlign=center
+    //                             link url='{key}' value='{iconName}' visible=false
+    //                             link url='{key}' value='{name}' visible=false
+    //                             button icon='Edit' title='Edit todo' width=16 height=16 visible=true data='{key}'
+    //                             button icon='Delete' iconColor=red title='Delete todo' width=16 height=16 visible=true data='{key}'
+    //                 items id=gridItems
+    //                     item key=1 name='Item 1' iconName='ItemIcon1' description='Descr A'
+    //                     item key=2 name='Item 2' iconName='ItemIcon2' description='Descr B'
+    //                     item key=3 name='Item 3' iconName='ItemIcon3' description='Descr C'
+    // `)
+
     await p.send(`
         add to=page at=0 text size=large bold=true value='Search MusicBrainz - The Open Music Encyclopedia' margin="1em 0em"
     `)
 
     await p.send(`
         add to=page at=1
-        stack id=search width=100% horizontal=true horizontalAlign=center 
-          textbox id=string label='Search for...' width=400px
-          dropdown id=dropdown label='By' width=200px
+        stack id=search width='100%' horizontal horizontalAlign='stretch' 
+          textbox id=string label='Search for...' width='400px'
+          dropdown id=dropdown label='By' width='200px'
             option key=artist text=artist
             option key=release text=album
           button id=button primary text=Search data=search_btn width=100px margin=2em        
@@ -59,9 +91,15 @@ if ('album' in args) {
 
     await p.send(`
         add to=page at=2
-        text size=medium bold=true value='Search Results'
-        stack id=results width=100%      
+            text size=medium bold=true value='Search Results'
+            stack id=spinner width=100% horizontal horizontalAlign='stretch'
+            
+            
     `)
+    // await p.send(`
+    //     add to=page at=3
+    //         stack id=spinner width=100% horizontal horizontalAlign='stretch'
+    // `)
 
     let searchType;
     while(true) {
--- node/music-search/mbApi.js
+++ node/music-search/mbApi.js
@@ -13,86 +13,45 @@ const mbApi = new MusicBrainzApi({
 async function queryArtists(p, searchString) {
 
     await p.send(`
-        add spinner to=results at=0 id=searchspinner label="search executing"
+        add spinner to=spinner id=searchspinner label="search executing"
     `) 
     
     let query = {};
     query['artist'] = searchString;
-
     let returnData = await mbApi.searchArtist(query, 0, 10);
-
     let returnArray = returnData['artists'];
-    console.log(returnArray);
-    
-    /* ? why doesn't this work ? 
-    
-    returnArray.forEach(async element => {
-        await p.send(`
-            add text value="${element.name}"
-        `)
-    });*/
-    
-    // add
-    // stack horizontal horizontalAlign='stretch'
-    // stack minwidth='250px'
-    //     nav
-    //     item text='Group 1'
-    //         item text='New'
-    //         item key='email' text='Email message' icon='Mail'
-    //         item key='calendar' text='Calendar event' icon='Calendar'
-    //     item text='Group 2'
-    //         item key=share text='Share' icon='Share'
-    //         item key=twitter text='Share to Twitter'
-    // stack width='100%' horizontal horizontalAlign='stretch'
-    //     grid compact=false selection=none preserveSelection headerVisible=true
-    //     columns
-    //         column onClick name='File Type' icon=Page iconOnly fieldName='iconName' minWidth=20 maxWidth=20
-    //         icon name=FileTemplate size=16
-    //         column resizable sortable name='Name' fieldName='name'
-    //         text value='{name}'
-    //         column resizable name='Write' fieldName='write'
-    //         stack horizontal height='100%' verticalAlign=center
-    //             checkbox value='{write}'
-    //         column resizable name='Color' fieldName='read'
-    //         dropdown value='{color}' data='{key}'
-    //             option key=red text='Red'
-    //             option key=green text='Green'
-    //             option key=blue text='Blue'
-    //         column resizable sortable name='Description' fieldName='description'
-    //         textbox value='{description}'
-    //         column sortable=number name='Action' fieldName='key' minWidth=150
-    //         stack horizontal height='100%' verticalAlign=center
-    //             link url='{key}' value='{iconName}' visible=false
-    //             link url='{key}' value='{name}' visible=false
-    //             button icon='Edit' title='Edit todo' width=16 height=16 visible=true data='{key}'
-    //             button icon='Delete' iconColor=red title='Delete todo' width=16 height=16 visible=true data='{key}'
-    //     items id=gridItems
-    //         item key=1 name='Item 1' iconName='ItemIcon1' description='Descr A'
-    //         item key=2 name='Item 2' iconName='ItemIcon2' description='Descr B'
-    //         item key=3 name='Item 3' iconName='ItemIcon3' description='Descr C'
-
+    console.log(returnArray);  
     await p.send(`
-        add 
-            grid compact=false selection=none preserveSelection headerVisible=true
-            columns
-                column name='Name' fieldName='name' resizable sortable
-                text value='{name}'
-                column resizable sortable name='Type' fieldName='type'
-                textbox value='{description}'
-            items id=gridItems
-    `)
-    for (i=0; i < returnArray.length; i++) {
-        // await p.send(`
-        //  add to=gridItems
-        //     item name="${returnArray[i].name}" description="${returnArray[i].type}"
+        clean results:gridItems
 
-        // `)
+    `)
+    await p.send(`  
+        add 
+        stack width='100%' id=results
+            grid compact=false selection=none preserveSelection headerVisible=true
+                columns
+                    column resizable sortable name='Name' fieldName='name' 
+                        text value='{name}'
+                    column resizable sortable name='Type' fieldName='type'
+                        text value='{type}'
+                    column resizable sortable name='Origin' fieldName='origin' 
+                        text value='{origin}'
+                    column resizable sortable name='Id' fieldName='artistId'
+                        text value='{artistId}'
+                items id=gridItems
+
+    `)
+
+    for (i=0; i < returnArray.length; i++) {
+
         await p.send(`
-            add text to=results value="${returnArray[i].name}"
+            add to=results:gridItems 
+                item name="${returnArray[i].name}" type="${returnArray[i].type}" origin="${returnArray[i].country}" artistId="${returnArray[i].id}"
         `)
     }
+
     await p.send(`
-        remove results:searchspinner
+        remove spinner:searchspinner
     `)
 
 }
